81264a5faa66d7dd5da599e5cda205b613a5ef03
Павел Смирнов <Pasha@MacBook-Air-Pavel.local>
2020-01-08 02:06:02 +0300

    [08.12.2020 - Learn Functional Programming && Decorators]

---
+++ errors_handling.py
@@ -0,0 +1,19 @@
+##ERRORS HANDLING
+#print(1 + 'hello!')
+
+while True:
+    try:
+        age = int(input('What is your age? '))
+        10 / age
+    except ValueError:
+        print('please enter a number.')
+        continue
+    except ZeroDivisionError:
+        print('Enter age higher than 0')
+        break
+    else:
+        print('thanks')
+        break
+    finally:
+        print('ok, i am finally done.')
+    print('Can you hear me?')
